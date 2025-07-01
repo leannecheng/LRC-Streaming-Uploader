720e097172b048447fa36580102d242c3aac7c44
Leanne <leannech@umich.edu>
2025-06-30 19:38:17 -0500

    Changed query method

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ flow = Flow.from_client_config(
     redirect_uri=REDIRECT_URI,
 )
 
-query_params = st.experimental_get_query_params()
+query_params = st.query_params
 if "code" in query_params:
     code = query_params["code"][0]
     try:
